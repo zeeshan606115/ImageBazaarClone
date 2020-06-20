771192776cc33b350df48f7f25feed302d309990
unknown <zeeshan.husain54@gmail.com>
2020-06-20 22:31:26 +0530

    UI has changed

--- imagebazar/settings.py
+++ imagebazar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '95ivp29x4go3$nbdu970bjl2)%231^%wq_g!smtz(=$g91+5nc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['awesomephotography.herokuapp.com']
+ALLOWED_HOSTS = ['awesomephotography.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
