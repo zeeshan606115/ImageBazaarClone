4015a98663c46562746d61539667136f839caf85
unknown <zeeshan.husain54@gmail.com>
2020-06-20 13:25:44 +0530

    modification in UI

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from wsgiref.util import FileWrapper
+import mimetypes
+import os
 # Create your views here.
 
 def show_home_page(request):
@@ -18,4 +21,15 @@ def show_category(request, cid):
 
 def home(request):
     return redirect('/show_home_page')
+
+
+
+def download_image(request, image_id):
+    img = Image.objects.get(id=image_id)
+    wrapper = FileWrapper(open(img.file))  # img.file returns full path to the image
+    content_type = mimetypes.guess_type(filename)[0]  # Use mimetypes to get file type
+    response = HttpResponse(wrapper,content_type=content_type)
+    response['Content-Length'] = os.path.getsize(img.file)
+    response['Content-Disposition'] = "attachment; filename=%s" %  img.name
+    return response
 
